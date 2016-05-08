22c31dee65061a8d5f67d1ef3b9a83cf5317b1b3
shamrock-frost <shamrockfrost@gmail.com>
2016-05-07 19:34:12 -0700

    Styles class created. Currently working on a dummy gui to learn TornadoFX

--- src/main/java/com/rhs/murphyTCG/AppMain.kt
+++ src/main/java/com/rhs/murphyTCG/AppMain.kt
@@ -1,7 +1,15 @@
 package com.rhs.murphyTCG
 
 import tornadofx.App
+import tornadofx.importStylesheet
+import tornadofx.reloadStylesheetsOnFocus
 
 class AppMain : App(){
     override val primaryView = BattleScreen::class
+
+    init {
+        importStylesheet(Styles::class)
+
+        reloadStylesheetsOnFocus()
+    }
 }
--- src/main/java/com/rhs/murphyTCG/BattleScreen.kt
+++ src/main/java/com/rhs/murphyTCG/BattleScreen.kt
@@ -1,17 +1,36 @@
 package com.rhs.murphyTCG
 
+import com.rhs.murphyTCG.Styles.Companion.wrapper
+import com.rhs.murphyTCG.Styles.Companion.password
+import com.rhs.murphyTCG.Styles.Companion.username
+import com.rhs.murphyTCG.Styles.Companion.row
 import javafx.scene.layout.VBox
-import tornadofx.View
-import tornadofx.hbox
-import tornadofx.label
+import tornadofx.*
 
 class BattleScreen : View() {
     override val root = VBox()
 
     init {
+        title = "Pokemon: the Gatheringioh, Heroes of Vanguard"
+
         with(root) {
+            addClass(wrapper)
+            children.addClass(row)
+
             hbox {
-                label("Hello World!")
+                addClass(username)
+                label("Username")
+                textfield()
+            }
+
+            hbox {
+                addClass(password)
+                label("Password")
+                textfield()
+            }
+
+            hbox {
+                button("Login")
             }
         }
     }
